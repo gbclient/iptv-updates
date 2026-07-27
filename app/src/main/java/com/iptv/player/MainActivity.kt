aa7d1993c39e3776336600159f18778431dac9ea
gbclient <gbclient@users.noreply.github.com>
2026-07-27 02:05:52 +0200

    v18 - Fix icona tema, bump versione

--- app/src/main/java/com/iptv/player/MainActivity.kt
+++ app/src/main/java/com/iptv/player/MainActivity.kt
@@ -33,7 +33,7 @@ import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
-    companion object { const val VERSION = 17 }
+    companion object { const val VERSION = 18 }
 
     enum class ContentType { LIVE, VOD, SERIES }
 
