798bb1823fea3d315d05190ee2116a832c4f013c
Manuel <manuel@email.com>
2026-07-29 19:35:43 +0200

    v52 - Parsing JSON Stalker flessibile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.iptv.player"
         minSdk = 22
         targetSdk = 35
-        versionCode = 51
-        versionName = "51.0"
+        versionCode = 52
+        versionName = "52.0"
     }
 
     signingConfigs {
--- app/src/main/java/com/iptv/player/MainActivity.kt
+++ app/src/main/java/com/iptv/player/MainActivity.kt
@@ -33,7 +33,7 @@ import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
-    companion object { const val VERSION = 51 }
+    companion object { const val VERSION = 52 }
 
     enum class ContentType { LIVE, VOD, SERIES }
 
